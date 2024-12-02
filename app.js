336f8a3ab5860d12b688744775992b4448e7b7e2
brobo1 <armanahz95@gmail.com>
2024-12-02 15:14:06 +0100

    Refactor session management and update Prisma schema.
    Replace `expressSession` with `session` for consistency in `app.js`. Add a new `Session` model to the Prisma schema to handle session data with proper database representation.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const expressSession = require("express-session");
+const session = require("express-session");
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 
 app.use(
-  expressSession({
+  session({
     cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
     secret: "a santa at nasa",
     resave: true,
