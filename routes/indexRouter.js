13ec7c0bd038a9ecb58078cbb7809106dd9de101
Brobo1 <armanahz95@gmail.com>
2024-12-03 14:15:23 +0100

    Export indexRouter in indexRouter.js
    Move initialization of indexRouter to improve code readability and ensure it is exported properly. This change fixes a missing module export, enabling other parts of the application to utilize the indexRouter.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
-const indexRouter = Router();
 const indexController = require("../controllers/indexController");
+const indexRouter = Router();
 
 indexRouter.get("/", indexController.indexGet);
+
+module.exports = indexRouter;
