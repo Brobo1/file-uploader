e27953172e2f83ca4b3cb7924b5a7bb007fa9c06
Brobo1 <armanahz95@gmail.com>
2024-12-03 13:54:03 +0100

    Add Prisma client and local authentication strategy
    Introduce a new Prisma client in `prismaClient.js` for database interactions. Implement a local authentication strategy in `passport.js` using Passport.js and bcrypt for user validation through username and password.

---
+++ config/passport.js
@@ -0,0 +1,46 @@
+const { Strategy: LocalStrategy } = require("passport-local");
+const bcrypt = require("bcryptjs");
+const { prisma } = require("../db/prismaClient");
+
+exports.initPass = (passport) => {
+  passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = await prisma.user.findFirst({
+          where: { username: username },
+        });
+
+        if (!user) {
+          console.log("Incorrect username");
+          return done(null, false, { message: "Incorrect username" });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) {
+          console.log("Incorrect password");
+          return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+      } catch (e) {
+        return done(e);
+      }
+    }),
+  );
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+        id,
+      ]);
+      const user = rows[0];
+
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+};
