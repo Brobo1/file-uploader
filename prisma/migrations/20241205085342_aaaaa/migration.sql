b232552c909dff9c71d8289c1ecbd5b550b7e1ea
brobo1 <armanahz95@gmail.com>
2024-12-05 15:03:01 +0100

    Add "Folder" table and update schema relationships
    This migration introduces a new "Folder" table and modifies existing database relationships. The "File" table is updated with "createdAt" and "folderId" columns, and its "extension" column is made optional. Additionally, the "member" column is removed from the "User" table, and necessary foreign key constraints are added to ensure relational integrity.

---
+++ prisma/migrations/20241205085342_aaaaa/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `member` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "File" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "folderId" INTEGER,
+ALTER COLUMN "extension" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "member";
+
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+    "parentId" INTEGER,
+    "path" TEXT NOT NULL,
+
+    CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
