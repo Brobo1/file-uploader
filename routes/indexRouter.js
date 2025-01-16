b84ae9be350eb4c8c10f656cfcb81ea85d81e63c
Brobo1 <armanahz95@gmail.com>
2025-01-17 00:45:08 +0100

    `refactor(routes): remove unused multer upload route from indexRouter`

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,7 @@
 const { Router } = require("express");
 const indexController = require("../controllers/indexController");
 const indexRouter = Router();
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+
 const validator = require("../controllers/validator");
 
 indexRouter.get("/", indexController.indexGet);
@@ -22,11 +21,5 @@ indexRouter.post(
 );
 
 indexRouter.get("/logout", indexController.userLogoutGet);
-
-indexRouter.post(
-  "/upload",
-  upload.single("file_upload"),
-  indexController.uploadPost,
-);
 
 module.exports = indexRouter;
