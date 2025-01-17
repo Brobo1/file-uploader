aeae65bd6c12401da3354f4f0244ad352d9528c3
Brobo1 <armanahz95@gmail.com>
2025-01-17 20:52:12 +0100

    `fix: serve static files from 'public' directory`

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
   res.locals.user = req.user;
