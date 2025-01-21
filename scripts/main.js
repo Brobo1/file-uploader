c5ed9f6ae85dbad38d55bb4e20b3269a3f7ce4db
Brobo1 <armanahz95@gmail.com>
2025-01-21 21:29:56 +0100

    `feat: add Alpine.js via npm and initialize in main script`

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
 import { loadingHandler } from "./loading.js";
 import { deleteHandler } from "./delete.js";
 import { fileHandler } from "./file.js";
+import { Alpine } from "alpinejs";
 
 window.deleteHandler = deleteHandler;
 window.fileHandler = fileHandler;
+window.Alpine = Alpine;
+Alpine.start();
 
 document.addEventListener("DOMContentLoaded", () => {
   loadingHandler();
