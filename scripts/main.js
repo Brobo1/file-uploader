7312b41bbf9b01d0c75b8f220d2e455d81d53a5c
Brobo1 <armanahz95@gmail.com>
2025-01-22 00:57:58 +0100

    `feat: add renameHandler support to global scope`

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,12 @@ import { loadingHandler } from "./loading.js";
 import { deleteHandler } from "./delete.js";
 import { fileHandler } from "./file.js";
 import { Alpine } from "alpinejs";
+import { renameHandler } from "./rename.js";
 
 window.deleteHandler = deleteHandler;
 window.fileHandler = fileHandler;
+window.renameHandler = renameHandler;
+
 window.Alpine = Alpine;
 Alpine.start();
 
